607190a0d5497967d3451017d2450fdd7bdb649d
gamorris246 <40498429@live.napeir.ac.uk>
2021-04-20 21:59:23 -0400

    Changes to flexbox design and overlay

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 var count = 3;
+var userinput = localStorage.getItem("uinput");
 
 
 function refuse_iroh() {
-  document.getElementById("refuse_count").innerHTML = count++;
-  if (count > 5) 
-  {
-	  document.getElementById("iroh_message").innerHTML = "Iroh says: You sure are persistent!";
-  }
-  if (count > 10) 
-  {
-	  document.getElementById("iroh_message").innerHTML = "Iroh laughs heartily: Fine then I am no longer asking you. This is a command. Come have tea with me!";
-  }
+	document.getElementById('refuse_count').innerHTML = count++;
+	if (count > 5) 
+		{
+		  document.getElementById('iroh_message').innerHTML = "Iroh says: You sure are persistent!";
+		}
+	if (count > 10) 
+		{
+		  document.getElementById('iroh_message').innerHTML = "Iroh laughs heartily: Fine then I am no longer asking you. This is a command. Come train!";
+		}
+}
+
+function store_name()
+{
+	var user = document.getElementById('username').value;
+	console.log(user);
+	sessionStorage.setItem("username", user);
+}
+
+
+window.onload = function show_name(){
+	document.getElementById('username').innerHTML = sessionStorage.getItem('username');
 }

2101236272ff1edf48e1e5e1554ce18af1beb6e6
gamorris246 <40498429@live.napeir.ac.uk>
2021-04-23 00:01:46 -0400

    Story elements completed for Fire, Air, and Earth

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function validateForm(){
 }
 
 function random_name(){
-	const name_list = ["Xiao", "Tien", "Yuan", "Jiang", "Shu", "Paku", "Zang", "Gohan", "Krillin", "Ash", "Tom"];
+	const name_list = ["Xiao", "Tien", "Juan", "Pierce", "Ash", "Raiden"];
 	var random_name = name_list[Math.floor(Math.random() * name_list.length)];
 	document.getElementById('user_name').value = random_name;
 }
@@ -41,12 +41,22 @@ function technique(learnt){
 	sessionStorage.setItem("technique", learnt);
 	if (sessionStorage.getItem('technique') === "calm")
 		{	
-			document.getElementById('technique').innerHTML = " will train to master the breath of fire technique -- A breathing technique that focuses on control of mind and body. It can also help regulate body heat in freezing temperatures. <br>Are you sure you wish to learn this technique?";
+			document.getElementById('technique').innerHTML = " will train to master the breath of fire technique <br> A breathing technique that focuses on control of mind and body. It can also regulate body heat in freezing temperatures. <br>Are you sure you wish to learn this technique?";
 		}
 		
 	if (sessionStorage.getItem('technique') === "rage")
 		{	
-			document.getElementById('technique').innerHTML = " will train to master the Dragon's Rampage technique -- A powerful wide area attack that radiates uncontrollable fire outward. Laying waste to anything and anyone nearby whether friend or foe. <br>Are you sure you wish to learn this technique?";
+			document.getElementById('technique').innerHTML = " will train to master the Dragon's Rampage technique. <br> A powerful wide area attack that radiates uncontrollable fire outward. Laying waste to anything and anyone nearby whether friend or foe. <br>Are you sure you wish to learn this technique?";
+		}
+	
+	if (sessionStorage.getItem('technique') === "armour")
+		{	
+			document.getElementById('technique').innerHTML = "Earth Armour.<br> A common defensive skill that involves coating the user's body in a thick layer of earth to create a rather durable suit of armor at the cost of lower speed and flexibility. <br>Are you sure you wish to learn this technique?";
+		}
+	
+	if (sessionStorage.getItem('technique') === "sense")
+		{	
+			document.getElementById('technique').innerHTML = "Seismic Sense.<br> Allows the the user to sense even the smallest vibrations in the earth that they're standing on to construct a mental image of their surroundings. <br>Are you sure you wish to learn this technique?";
 		}
 	
 	if (sessionStorage.getItem('technique') === "")
@@ -55,22 +65,38 @@ function technique(learnt){
 		}
 }
 
-function stealth(){
-	if (sessionStorage.getItem('technique') == "calm")
+function use_technique(){
+	if (sessionStorage.getItem('technique') === "calm")
 		{	
 			document.getElementById('battle_option').innerHTML = "Your controlled breath of fire technique stealthily raises your internal temperature and allows you to stay in position without freezing";
 		}
 		
-	if (sessionStorage.getItem('technique') == "rage")
+	if (sessionStorage.getItem('technique') === "rage")
 		{	
 			window.open('endfire1.html', '_self');
 		}
+		
+	if (sessionStorage.getItem('technique') === "sense")
+		{	
+			window.open('battle_sense.html', '_self');
+		}
+		
+	if (sessionStorage.getItem('technique') === "armour")
+	{	
+		window.open('battle_armour.html', '_self');
+	}
 	
-	if (sessionStorage.getItem('technique') !== "calm" && sessionStorage.getItem('technique') !== "rage")
+	else
+		{	
+			window.open('no_technique_end.html', '_self');
+		}
+	
+	/*if (sessionStorage.getItem('technique') !== "calm" && sessionStorage.getItem('technique') !== "rage")
 		{	
 			window.open('endfire2.html', '_self');
-		}
+		}	*/
 }
+
 
 function clearStorage() {
   sessionStorage.clear();
